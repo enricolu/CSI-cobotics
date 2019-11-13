8a1e2eb77822fd169b4d0a176eb4127b567893db
douthwja01 <douthwaiteja@gmail.com>
2019-11-13 18:05:23 +0000

    Ammended camera controls, object focus and multi-camera support

---
+++ unity/.scripts/InspectionMovement.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class InspectionMovement : MonoBehaviour
+{
+    [Header("Movement Parameters")]
+
+    // Rotational sensitivity
+    [Tooltip("Multiplier for camera sensitivity.")]
+    [Range(0f, 10)]
+    public float mouseSensitivity = 5.0f; //160f;
+    // Translational sensitivity
+    [Tooltip("Multiplier for camera translation.")]
+    [Range(0f, 10f)]
+    public float movementSensitivity = 5.0f;
+    // Scroll sensitivity
+    [Tooltip("Multiplier for camera translation.")]
+    [Range(0f, 10f)]
+    public float zoomSensitivity = 5.0f;
+
+    // Private references
+    private float rotationSensitivity;
+    private float translationSensitivity;
+    // Containers
+    private Vector2 cameraRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // LateUpdate is called every frame, if the Behaviour is enabled
+    private void LateUpdate()
+    {
+        // Scale the sensitivities
+        rotationSensitivity     = 320f * (mouseSensitivity / 10f);
+        translationSensitivity  = 40f * (movementSensitivity / 10f);
+
+        // Hold mouse 2 for rotation behaviour
+        if (Input.GetMouseButton(1))
+        {
+            Debug.Log("Mouse 2 held");
+            PointRotation(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+            return;
+        }
+        
+        // Hold mouse 3 for translation behaviour
+        if (Input.GetMouseButton(2))
+        {
+            Debug.Log("Mouse 3 held");
+            PlanarTranslation(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+            return;
+        }
+
+
+        float scrollDelta = Input.GetAxis("Mouse ScrollWheel");
+        if (scrollDelta != 0f)
+        {
+            Debug.Log("Scrolling");
+            AxialZoom(scrollDelta);
+        }
+
+        // Nominal translation behaviour
+        PointTranslation(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"),0f);
+    }
+
+    // Zoom         - Zoom along axis by factor
+    private void AxialZoom(float zoomDelta)
+    {
+        // Translate along the forward axis to emulate zoom
+        transform.Translate(Vector3.forward * zoomSensitivity * zoomDelta);
+    }
+    // Translation  - XYZ translation from WASD
+    private void PointTranslation(float xAxis,float yAxis,float zAxis)
+    {
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            transform.position += transform.up * translationSensitivity * Time.deltaTime;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Z))
+        {
+            transform.position -= transform.up * translationSensitivity * Time.deltaTime;
+        }
+        return;
+    }
+    // Rotation     - Point rotation
+    private void PointRotation(float xAxis,float yAxis)
+    {
+        // Calculate camera rotation   
+        cameraRotation.x += xAxis * rotationSensitivity * Time.deltaTime;
+        cameraRotation.y += yAxis * rotationSensitivity * Time.deltaTime;
+        // Apply camera rotation
+        transform.localRotation  = Quaternion.AngleAxis(cameraRotation.x, Vector3.up);
+        transform.localRotation *= Quaternion.AngleAxis(cameraRotation.y, Vector3.left);
+        return;
+    }
+    // Translation  - XY mouse to XY translation
+    private void PlanarTranslation(float xAxis,float yAxis)
+    {
+        // Modify camera position
+        transform.position += transform.right * translationSensitivity* xAxis * Time.deltaTime;
+        transform.position += transform.up * translationSensitivity* yAxis * Time.deltaTime;
+        return;
+    }
+}
---
+++ unity/.scripts/MultiCameraController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CSI;
+
+namespace CSI.user
+{
+    public class MultiCameraController : MonoBehaviour
+    {
+        // List camera options
+        public List<Camera> viewPoints;
+        private int index = 0;
+        // Display current camera
+        public Camera currentCamera;
+
+        // Start 
+        void Start()
+        {
+            // Initial camera selection
+            GetUniqueViewPoint(viewPoints[index]);
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                // Iterate
+                index += 1;
+                // Catch the vector end
+                if (index > (viewPoints.Count - 1))
+                {
+                    index = 0;
+                }
+
+                Debug.Log("Switching to camera: " + index);
+
+                // Cut to camera 
+                CutToIndex(index);
+            }
+        }
+        
+        // This function cycles through the List of view points
+        public void CutToIndex(int ind)
+        {
+            // Disable previous camera (only one can be active at once)
+            DisableCamera(currentCamera);
+            // Set new camera
+            EnableCamera(viewPoints[ind]);
+        }
+
+        // Ensure only one active camera
+        private void GetUniqueViewPoint(Camera uniqueCamera)
+        {
+            // Get the numer of active cameras
+            if (Camera.allCamerasCount > 1)
+            {
+                //Debug.Log("More than one camera active, getting unique view point.");
+                // Disable all cameras
+                Camera[] cameraVector = Camera.allCameras;
+                foreach (Camera unusedCamera in cameraVector)
+                {
+                    // Disable all other cameras
+                    DisableCamera(unusedCamera);
+                }
+            }
+            // Set the current camera to be the unique camera
+            EnableCamera(uniqueCamera);
+        }
+        // Enable camera
+        private void EnableCamera(Camera viewPoint)
+        {
+            viewPoint.enabled = true;
+            //viewPoint.gameObject.SetActive(false);
+            viewPoint.GetComponent<AudioListener>().enabled = true;
+            // Record as the current camera
+            currentCamera = viewPoint;
+        }
+        // Disable camera
+        private void DisableCamera(Camera viewPoint)
+        {
+            viewPoint.enabled = false;
+            viewPoint.GetComponent<AudioListener>().enabled = false;
+        }
+    } 
+}
